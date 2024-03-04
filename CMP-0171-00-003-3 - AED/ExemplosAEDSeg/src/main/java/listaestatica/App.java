c2b9f1a54e5569a8419a89017914c724e1e27350
André <andre@iburger.com.br>
2024-03-04 20:30:27 -0300

    ✨ feat: Exemplos listas estáticas

---
+++ CMP-0171-00-003-3 - AED/ExemplosAEDSeg/src/main/java/listaestatica/App.java
@@ -0,0 +1,37 @@
+package listaestatica;
+
+public class App {
+
+    public static void main(String[] args) {
+    	
+    	ListaEstatica listaEstatica = new ListaEstatica();
+    	
+    	System.out.println("Lista estática está vazia:" + listaEstatica.estaVazia());
+    	
+    	listaEstatica.inserir(5);
+    	listaEstatica.inserir(15);
+    	listaEstatica.inserir(-7);
+    	listaEstatica.inserir(21);
+    	listaEstatica.inserir(9);
+    	listaEstatica.inserir(10);
+    	listaEstatica.inserir(3);
+    	listaEstatica.inserir(47);
+    	listaEstatica.inserir(54);
+    	listaEstatica.inserir(81);
+    	listaEstatica.inserir(-4);
+    	listaEstatica.inserir(12);
+    	
+    	System.out.println("Lista estática está vazia:" + listaEstatica.estaVazia());
+    	
+    	listaEstatica.exibir();
+    	
+    	int indexBusca = listaEstatica.buscar(21);
+    	System.out.println("A posição do valor buscado é: " + indexBusca);
+    	
+    	
+    	listaEstatica.retirar(21);
+
+    	listaEstatica.exibir();
+    	
+    }
+}
---
+++ CMP-0171-00-003-3 - AED/ExemplosAEDSeg/src/main/java/listaestatica/ListaEstatica.java
@@ -0,0 +1,80 @@
+package listaestatica;
+
+public class ListaEstatica {
+	
+	// Define a lista que vai armazenar os valores
+	private int[] info;
+	// Variável que controla o tamanho da lista.
+	private int tamanho;
+	
+	// Inicia a lista
+	public ListaEstatica() {
+		info = new int[10];
+		tamanho = 0;
+	}
+
+	// Insere um valor na lista, verificando se precisa redimensionar ou não.
+	public void inserir(int valor) {
+		if (tamanho == info.length) {
+			redimensionar();
+		}
+		info[tamanho] = valor;
+		tamanho++;
+	}
+	
+	// Realiza o redimensionamento da lista, sempre incrementando 10 posições
+	private void redimensionar() {
+		int[] novaInfo = new int[info.length + 10];
+
+		for (int i=0; i<info.length; i++) {
+			novaInfo[i] = info[i];
+		}
+		
+		info = novaInfo;
+	}
+	
+	// Realiza a busca de um valor, e retorna a posição do mesmo.
+	public int buscar(int valor) {
+		for (int i=0; i<tamanho; i++) {
+			if (info[i] == valor) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	// Retira um elemento da lista
+	public void retirar(int valor) {
+		int posicao = buscar(valor);
+		
+		if (posicao > -1) {
+			for (int i=posicao; i<tamanho-1; i++) {
+				info[i] = info[i+1];
+			}
+			
+			tamanho--;
+		}
+	}
+	
+	// Exibe os elementos da lista
+	public void exibir() {
+		for (int i=0; i<tamanho; i++) {
+			System.out.println(info[i]);
+		}
+	}
+	
+	// Informa se a lista está vazia ou não.
+	public boolean estaVazia() {
+		return tamanho==0;
+	}
+	
+	public int getTamanho() {
+		return tamanho;
+	}
+	
+	public void liberar() {
+		info = new int[10];
+		tamanho = 0;
+	}
+}
